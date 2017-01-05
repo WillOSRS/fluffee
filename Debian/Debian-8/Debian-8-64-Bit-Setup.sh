0a7ac500cbfd41159824e8905b6049112a9f361d
Fluffee <hifluffee@outlook.com>
2017-01-04 21:02:38 -0330

    Debian Updates

--- Debian/Debian-8/Debian-8-64-Bit-Setup.sh
+++ Debian/Debian-8/Debian-8-64-Bit-Setup.sh
@@ -9,8 +9,9 @@ echo -n "Installing updates..."
 apt-get update &> /dev/null
 echo " Done"
 echo -n "Installing required packages..."
+apt-get -y upgrade &> /dev/null
 apt-get -y install sudo wget nano locales debconf-utils &> /dev/null
-wget --no-check-cert 'https://raw.githubusercontent.com/iFluffee/Fluffees-Server-Setup/master/Debian/Debian-8/Keyboard_settings.conf' &> /dev/null
+wget 'https://raw.githubusercontent.com/iFluffee/Fluffees-Server-Setup/master/Debian/Debian-8/Keyboard_settings.conf' &> /dev/null
 debconf-set-selections < Keyboard_settings.conf &> /dev/null
 apt-get install -y keyboard-configuration &> /dev/null
 dpkg-reconfigure keyboard-configuration -f noninteractive &> /dev/null
@@ -24,21 +25,21 @@ dpkg-reconfigure --frontend=noninteractive locales &> /dev/null
 update-locale LANG=en_US.UTF-8 &> /dev/null
 echo " Done"
 echo -n "Setting up SSH..."
-sed -i "s/Port 22/Port $sshport/g" /etc/ssh/sshd_config &> /dev/null
-echo "AllowUsers $name root" >> /etc/ssh/sshd_config &> /dev/null
-sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config &> /dev/null
+sed -i "s/Port 22/Port $sshport/g" /etc/ssh/sshd_config
+echo "AllowUsers $name root" >> /etc/ssh/sshd_config
+sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
 chmod 600 /etc/ssh/sshd_config &> /dev/null
 service ssh restart &> /dev/null
 echo " Done"
 echo -n "Installing LXDE..."
 apt-get -y install xorg lxde lxtask &> /dev/null
 sudo adduser $name --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password &> /dev/null
-echo "$name:$sshpassword" | sudo chpasswd &> /dev/null
+echo "$name:$sshpassword" | sudo chpasswd
 sudo gpasswd -a $name sudo &> /dev/null
 sudo gpasswd -a $name netdev &> /dev/null
 echo " Done"
 echo -n "Installing TightVNC 1.3.10 (Non broken version)..."
-sudo apt-get install -y xorg-dev libjpeg62-dev zlib1g-dev build-essential xutils-dev &> /dev/null
+sudo apt-get install -y xorg-dev libjpeg62-turbo-dev zlib1g-dev build-essential xutils-dev &> /dev/null
 wget http://www.tightvnc.com/download/1.3.10/tightvnc-1.3.10_unixsrc.tar.gz &> /dev/null
 tar xzf tightvnc-1.3.10_unixsrc.tar.gz &> /dev/null
 cd vnc_unixsrc &> /dev/null
@@ -49,18 +50,18 @@ cd Xvnc &> /dev/null
 make &> /dev/null
 cd .. &> /dev/null
 ./vncinstall /usr/local/bin /usr/local/man &> /dev/null
-sed -i -r '/unix\/:7100";/a $fontPath = join ',',qw(' /usr/local/bin/vncserver &> /dev/null
-sed -i "s/join ,,qw/join ',,qw/g" /usr/local/bin/vncserver &> /dev/null
-sed -i "s/join ',,qw/join ',',qw/g" /usr/local/bin/vncserver &> /dev/null
-sed -i /',qw(/a /usr/share/fonts/X11/misc' /usr/local/bin/vncserver &> /dev/null
-sed -i '/\/X11\/misc/a /usr/share/fonts/X11/100dp2i/:unscaled' /usr/local/bin/vncserver &> /dev/null
-sed -i '/\/usr\/share\/fonts\/X11\/100dp2i\/\:unscaled/a /usr/share/fonts/X11/75dp2i/:unscaled' /usr/local/bin/vncserver &> /dev/null
-sed -i '/\/usr\/share\/fonts\/X11\/75dp2i\/\:unscaled/a /usr/share/fonts/X11/Type1' /usr/local/bin/vncserver &> /dev/null
-sed -i '/\/usr\/share\/fonts\/X11\/Type1/a /usr/share/fonts/X11/100dpi' /usr/local/bin/vncserver &> /dev/null
-sed -i '/\/usr\/share\/fonts\/X11\/100dpi/a /usr/share/fonts/X11/75dpi' /usr/local/bin/vncserver &> /dev/null
-sed -i '/\/usr\/share\/fonts\/X11\/75dpi/a );' /usr/local/bin/vncserver &> /dev/null
-sed -i "s/75dp2i/75dpi/g" /usr/local/bin/vncserver &> /dev/null
-sed -i "s/100dp2i/100dpi/g" /usr/local/bin/vncserver &> /dev/null
+sed -i -r '/unix\/:7100";/a $fontPath = join ',',qw(' /usr/local/bin/vncserver
+sed -i "s/join ,,qw/join ',,qw/g" /usr/local/bin/vncserver
+sed -i "s/join ',,qw/join ',',qw/g" /usr/local/bin/vncserver
+sed -i /',qw(/a /usr/share/fonts/X11/misc' /usr/local/bin/vncserver
+sed -i '/\/X11\/misc/a /usr/share/fonts/X11/100dp2i/:unscaled' /usr/local/bin/vncserver
+sed -i '/\/usr\/share\/fonts\/X11\/100dp2i\/\:unscaled/a /usr/share/fonts/X11/75dp2i/:unscaled' /usr/local/bin/vncserver
+sed -i '/\/usr\/share\/fonts\/X11\/75dp2i\/\:unscaled/a /usr/share/fonts/X11/Type1' /usr/local/bin/vncserver
+sed -i '/\/usr\/share\/fonts\/X11\/Type1/a /usr/share/fonts/X11/100dpi' /usr/local/bin/vncserver
+sed -i '/\/usr\/share\/fonts\/X11\/100dpi/a /usr/share/fonts/X11/75dpi' /usr/local/bin/vncserver
+sed -i '/\/usr\/share\/fonts\/X11\/75dpi/a );' /usr/local/bin/vncserver
+sed -i "s/75dp2i/75dpi/g" /usr/local/bin/vncserver
+sed -i "s/100dp2i/100dpi/g" /usr/local/bin/vncserver
 echo " Done"
 echo -n "Setting up VNC..."
 mkdir /home/$name/.vnc
@@ -77,13 +78,13 @@ sed -i "s/xterm -geometry 80x24+10+10 -ls -title \"\$VNCDESKTOP Desktop\" \&//g"
 sed -i "s/twm/startlxde/g" /home/$name/.vnc/xstartup
 su  - $name -c "vncserver" &> /dev/null
 su  - $name -c "vncserver -kill :1" &> /dev/null
-cd /usr/local/bin/ &> /dev/null
+cd /usr/local/bin/
 wget --no-check-cert 'https://raw.githubusercontent.com/iFluffee/Fluffees-Server-Setup/master/Debian/Debian-8/myvncserver' &> /dev/null
 sudo chown $name myvncserver &> /dev/null
 sudo chmod +x /usr/local/bin/myvncserver &> /dev/null
-cd /lib/systemd/system/ &> /dev/null
+cd /lib/systemd/system/
 wget --no-check-cert 'https://raw.githubusercontent.com/iFluffee/Fluffees-Server-Setup/master/Debian/Debian-8/myvncserver.service' &> /dev/null
-sed -i "s/User=vnc/User=$name/g" /lib/systemd/system/myvncserver.service &> /dev/null
+sed -i "s/User=vnc/User=$name/g" /lib/systemd/system/myvncserver.service
 sudo systemctl daemon-reload &> /dev/null
 sudo systemctl enable myvncserver.service &> /dev/null
 sudo update-rc.d tightvncserver defaults &> /dev/null
@@ -92,12 +93,12 @@ echo -n "Downloading TRiBot and OSBuddy..."
 sudo mkdir /home/$name/Desktop/ &> /dev/null
 sudo mkdir /home/$name/Desktop/Bots/ &> /dev/null
 cd /home/$name/Desktop/
-sudo chown $name Bots
+sudo chown $name Bots &> /dev/null
 wget -O /home/$name/Desktop/Bots/TRiBot_Loader.jar https://tribot.org/bin/TRiBot_Loader.jar &> /dev/null
 wget -O /home/$name/Desktop/Bots/OSBuddy.jar http://cdn.rsbuddy.com/live/f/loader/OSBuddy.jar?x=10 &> /dev/null
 cd /home/$name/Desktop
-sudo chown $name Bots
-sudo chmod 777 Bots
+sudo chown $name Bots &> /dev/null
+sudo chmod 777 Bots &> /dev/null
 echo " Done"
 echo -n "Setting up Java..."
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list &> /dev/null
@@ -107,7 +108,7 @@ apt-get update &> /dev/null
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections &> /dev/null
 apt-get -y install oracle-java8-installer &> /dev/null
 sudo apt-get -y install oracle-java8-set-default &> /dev/null
-chmod 777 /usr/lib/jvm/java-8-oracle/jre/lib/security/java.policy &> /dev/null
+chmod 777 /usr/lib/jvm/java-8-oracle/jre/lib/security/java.policy
 cd /usr/local
 echo " Done"
 echo -n "Installing Firefox x64..."
