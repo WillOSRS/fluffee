61601f7c6673e8947886b59cc39adbf887a63dd4
Fluffee <hifluffee@outlook.com>
2017-01-12 21:28:48 -0330

    Fuse Updates

--- Debian/Debian-8/Debian-8-64-Bit-Setup.sh
+++ Debian/Debian-8/Debian-8-64-Bit-Setup.sh
@@ -34,6 +34,7 @@ echo " Done"
 echo -n "Installing LXDE..."
 apt-get install -y xorg-dev libjpeg62-turbo-dev zlib1g-dev build-essential xutils-dev &> /dev/null
 wget --no-check-cert 'https://github.com/libfuse/libfuse/releases/download/fuse-3.0.0/fuse-3.0.0.tar.gz' &> /dev/null
+tar xzf fuse-3.0.0.tar.gz &> /dev/null
 cd fuse-3.0.0
 ./configure --prefix=/usr --disable-static INIT_D_PATH=/tmp/init.d && make &> /dev/null
 make install &> /dev/null
