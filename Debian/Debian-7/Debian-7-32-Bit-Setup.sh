14ca29e852f4b7ebfaec8ced6f3bdf7ca0648fdd
Fluffee <hifluffee@outlook.com>
2017-04-09 23:19:01 -0230

    Test

--- Debian/Debian-7/Debian-7-32-Bit-Setup.sh
+++ Debian/Debian-7/Debian-7-32-Bit-Setup.sh
@@ -39,8 +39,8 @@ sudo adduser $name --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disable
 echo "$name:$sshpassword" | sudo chpasswd &> /dev/null
 sudo adduser $name sudo &> /dev/null
 sudo adduser $name netdev &> /dev/null
-DEBIAN_FRONTEND=noninteractive apt-get -yq install xorg
-DEBIAN_FRONTEND=noninteractive apt-get -yq install lxtask
+DEBIAN_FRONTEND=noninteractive apt-get -yq install xorg &> /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get -yq install lxtask &> /dev/null
 DEBIAN_FRONTEND=noninteractive apt-get -yq install lxde
 echo " Done"
 echo -n "Installing TightVNC 1.3.10 (Non broken version)..."
