a69232e2138855127df032ac1d45c3810b4452bc
Fluffee <hifluffee@outlook.com>
2017-11-01 00:56:05 -0230

    v2.2.3 - Whoops, forgot a line.

--- Debian/Debian-7/Debian-7-32-Bit-Setup.sh
+++ Debian/Debian-7/Debian-7-32-Bit-Setup.sh
@@ -129,9 +129,12 @@ cd
 # source /etc/profile.d/oraclejdk.sh
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list &> /dev/null
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list &> /dev/null
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections &> /dev/null
+echo oracle-java8-installer shared/accepted-oracle-licence-v1-1 boolean true | sudo /usr/bin/debconf-set-selections &> /dev/null
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886 &> /dev/null
 apt-get update &> /dev/null
 apt-get install -yq oracle-java8-installer &> /dev/null
+apt-get install -yq oracle-java8-set-default &> /dev/null
 echo " Done"
 echo -n "Installing Firefox x86..."
 cd /usr/local
--- Debian/Debian-8/Debian-8-64-Bit-Setup-verbose.sh
+++ Debian/Debian-8/Debian-8-64-Bit-Setup-verbose.sh
@@ -143,9 +143,12 @@ cd
 # source /etc/profile.d/oraclejdk.sh
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+echo oracle-java8-installer shared/accepted-oracle-licence-v1-1 boolean true | sudo /usr/bin/debconf-set-selections
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 apt-get update
 apt-get install -yq oracle-java8-installer
+apt-get install -yq oracle-java8-set-default
 echo " Done"
 echo -n "Installing Firefox x64..."
 cd /usr/local
