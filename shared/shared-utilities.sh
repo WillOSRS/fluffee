83df00ac1eaa2ed9eb4d3af9e12724db8d74248e
Fluffee <hifluffee@outlook.com>
2019-05-12 19:57:02 -0230

    Missed permissions on file to allow it to run on boot

--- shared/shared-utilities.sh
+++ shared/shared-utilities.sh
@@ -47,6 +47,7 @@ function setup_vnc_initd_service() {
   sed -i "s/user_name/$name/g" /etc/init.d/vncserver
   chmod +x /etc/init.d/vncserver
   echo "service vncserver start" >> /etc/rc.d/rc.local
+  chmod +x /etc/rc.d/rc.local
 }
 
 # Creates the tiger vnc systemd service
